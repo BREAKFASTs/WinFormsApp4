4a7e47f01845d353bfd2bd1ef5e0673dcae850fc
YsabelleM <52094755+YsabelleM@users.noreply.github.com>
2024-12-09 06:48:25 +0800

    Add UpdateQuantity helper method

--- WinFormsApp4/Order.cs
+++ WinFormsApp4/Order.cs
@@ -30,187 +30,166 @@ namespace WinFormsApp4
 
         private void btnSubtractCafeAmericano_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtCafeAmericano_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtCafeAmericano, -1, 0, 999);
         }
 
         private void btnAddCafeAmericano_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtCafeAmericano, 1, 0, 999);
         }
 
         private void btnSubtractCaramelMacchiato_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtCaramelMacchiato_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtCaramelMacchiato, -1, 0, 999);
         }
 
         private void btnAddCaramelMacchiato_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtCaramelMacchiato, 1, 0, 999);
         }
 
         private void btnSubtractPumpkinSpiceLatte_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtPumpkinSpiceLatte_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtPumpkinSpiceLatte, -1, 0, 999);
         }
 
         private void btnAddPumpkinSpiceLatte_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtPumpkinSpiceLatte, 1, 0, 999);
         }
 
         private void btnSubtractFlatWhite_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtFlatWhite_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtFlatWhite, -1, 0, 999);
         }
 
         private void btnAddFlatWhite_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtFlatWhite, 1, 0, 999);
         }
 
         private void btnSubtractMocha_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtMocha_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtMocha, -1, 0, 999);
         }
 
         private void btnAddMocha_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtMocha, 1, 0, 999);
         }
 
         private void btnSubtractIcedCoffee_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtIcedCoffee_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtIcedCoffee, -1, 0, 999);
         }
 
         private void btnAddIcedCoffee_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtIcedCoffee, 1, 0, 999);
         }
 
         private void btnSubtractChaiLatte_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtChaiLatte_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtChaiLatte, -1, 0, 999);
         }
 
         private void btnAddChaiLatte_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtChaiLatte, 1, 0, 999);
         }
 
         private void btnSubtractCappucino_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtCappucino_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtCappucino, -1, 0, 999);
         }
 
         private void btnAddCapuccino_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtCappucino, 1, 0, 999);
         }
 
         private void btnSubtractIcedMatchaLatte_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtIcedMatchaLatte_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtIcedMatchaLatte, -1, 0, 999);
         }
 
         private void btnAddIcedMatchaLatte_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtIcedMatchaLatte, 1, 0, 999);
         }
 
         private void btnSubtractStrawberryAcaiRefresher_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtStrawberryAcaiRefresher_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtStrawberryAcaiRefresher, -1, 0, 999);
         }
 
         private void btnAddStrawberryAcaiRefresher_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtStrawberryAcaiRefresher, 1, 0, 999);
         }
 
         private void btnSubtractNitroColdBrew_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void txtNitroColdBrew_TextChanged(object sender, EventArgs e)
-        {
-
+            UpdateQuantity(txtNitroColdBrew, -1, 0, 999);
         }
 
         private void btnAddNitroColdBrew_Click(object sender, EventArgs e)
         {
-
+            UpdateQuantity(txtNitroColdBrew, 1, 0, 999);
         }
 
-        private void txtOrderInformation_TextChanged(object sender, EventArgs e)
+        // Helper method to update the quantity in TextBox
+        private void UpdateQuantity(TextBox textBox, int change, int min, int max)
         {
-
+            if (int.TryParse(textBox.Text, out int currentValue))
+            {
+                currentValue += change;
+                if (currentValue < min)
+                {
+                    currentValue = min;
+                }
+                else if (currentValue > max)
+                {
+                    currentValue = max;
+                }
+                textBox.Text = currentValue.ToString();
+            }
+            else
+            {
+                textBox.Text = "0"; // Reset to 0 if invalid input
+            }
         }
 
-        private void btnClear_Click(object sender, EventArgs e)
-        {
+        // The following are the TextChanged event handlers
+        private void txtCafeAmericano_TextChanged(object sender, EventArgs e) { }
 
-        }
+        private void txtCaramelMacchiato_TextChanged(object sender, EventArgs e) { }
 
-        private void btnOrder_Click(object sender, EventArgs e)
-        {
+        private void txtPumpkinSpiceLatte_TextChanged(object sender, EventArgs e) { }
 
-        }
+        private void txtFlatWhite_TextChanged(object sender, EventArgs e) { }
 
-        private void txtTotalPurchased_TextChanged(object sender, EventArgs e)
-        {
+        private void txtMocha_TextChanged(object sender, EventArgs e) { }
 
-        }
+        private void txtIcedCoffee_TextChanged(object sender, EventArgs e) { }
+
+        private void txtChaiLatte_TextChanged(object sender, EventArgs e) { }
+
+        private void txtCappucino_TextChanged(object sender, EventArgs e) { }
+
+        private void txtIcedMatchaLatte_TextChanged(object sender, EventArgs e) { }
+
+        private void txtStrawberryAcaiRefresher_TextChanged(object sender, EventArgs e) { }
+
+        private void txtNitroColdBrew_TextChanged(object sender, EventArgs e) { }
+
+        private void txtOrderInformation_TextChanged(object sender, EventArgs e) { }
+
+        private void txtTotalPurchased_TextChanged(object sender, EventArgs e) { }
+
+        private void btnClear_Click(object sender, EventArgs e) { }
+
+        private void btnOrder_Click(object sender, EventArgs e) { }
+
     }
 }
--- WinFormsApp4/obj/Debug/net8.0-windows/WinFormsApp4.AssemblyInfo.cs
+++ WinFormsApp4/obj/Debug/net8.0-windows/WinFormsApp4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00875a0db9b985864a18931869b7765347a4035f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2065c7678ca108eae95530d869189a015bfba432")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
